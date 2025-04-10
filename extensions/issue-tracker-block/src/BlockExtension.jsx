545ac01c62ac4ccd8d1658f354720d8f72dddced
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-10 16:20:31 -0500

    extract funcs to utils file

--- extensions/issue-tracker-block/src/BlockExtension.jsx
+++ extensions/issue-tracker-block/src/BlockExtension.jsx
@@ -2,6 +2,7 @@
 
 import { render } from "preact";
 import { useEffect, useMemo, useState } from "preact/hooks";
+import { updateIssues, getIssues } from "./utils";
 
 export default function extension() {
   render(<Extension />, document.body);
@@ -21,6 +22,7 @@ function Extension() {
   const issuesCount = issues.length;
   const totalPages = issuesCount / PAGE_SIZE;
 
+  // [START build-admin-block.connect-api-one]
   useEffect(() => {
     (async function getProductInfo() {
       // Load the product's metafield of type issues
@@ -132,74 +134,4 @@ function Extension() {
       </s-form>
     </s-admin-block>
   );
-}
-
-export async function updateIssues(id, newIssues) {
-  // This example uses metafields to store the data. For more information, refer to https://shopify.dev/docs/apps/custom-data/metafields.
-  return await makeGraphQLQuery(
-    `mutation SetMetafield($namespace: String!, $ownerId: ID!, $key: String!, $type: String!, $value: String!) {
-    metafieldDefinitionCreate(
-      definition: {namespace: $namespace, key: $key, name: "Tracked Issues", ownerType: PRODUCT, type: $type, access: {admin: MERCHANT_READ_WRITE}}
-    ) {
-      createdDefinition {
-        id
-      }
-    }
-    metafieldsSet(metafields: [{ownerId:$ownerId, namespace:$namespace, key:$key, type:$type, value:$value}]) {
-      userErrors {
-        field
-        message
-        code
-      }
-    }
-  }
-  `,
-    {
-      ownerId: id,
-      namespace: "$app:issues",
-      key: "issues",
-      type: "json",
-      value: JSON.stringify(newIssues),
-    },
-  );
-}
-
-export async function getIssues(productId) {
-  // This example uses metafields to store the data. For more information, refer to https://shopify.dev/docs/apps/custom-data/metafields.
-  return await makeGraphQLQuery(
-    `query Product($id: ID!) {
-      product(id: $id) {
-        metafield(namespace: "$app:issues", key:"issues") {
-          value
-        }
-        variants(first: 2) {
-          edges {
-            node {
-              id
-            }
-          }
-        }
-      }
-    }
-  `,
-    { id: productId },
-  );
-}
-
-async function makeGraphQLQuery(query, variables) {
-  const graphQLQuery = {
-    query,
-    variables,
-  };
-
-  const res = await fetch("shopify:admin/api/graphql.json", {
-    method: "POST",
-    body: JSON.stringify(graphQLQuery),
-  });
-
-  if (!res.ok) {
-    console.error("Network error");
-  }
-
-  return await res.json();
 }
