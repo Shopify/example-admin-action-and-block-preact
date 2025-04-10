210e538fa51253742c0d09383d027702538d3d55
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-10 11:40:41 -0500

    extract funcs to utils file

--- extensions/issue-tracker-action/src/ActionExtension.jsx
+++ extensions/issue-tracker-action/src/ActionExtension.jsx
@@ -2,6 +2,7 @@
 // [START build-admin-action.create-ui-one]
 import { render } from "preact";
 import { useCallback, useEffect, useState } from "preact/hooks";
+import { getIssues, updateIssues } from "./utils";
 // [END build-admin-action.create-ui-one]
 
 // [START build-admin-action.create-ui-two]
@@ -9,6 +10,20 @@ export default function extension() {
   render(<Extension />, document.body);
 }
 // [END build-admin-action.create-ui-two]
+
+function generateId(allIssues) {
+  return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
+}
+
+function validateForm({ title, description }) {
+  return {
+    isValid: Boolean(title) && Boolean(description),
+    errors: {
+      title: !title,
+      description: !description,
+    },
+  };
+}
 
 function Extension() {
   const { close, data } = shopify;
@@ -79,85 +94,4 @@ function Extension() {
     </s-admin-action>
   );
   // [END build-admin-action.create-ui-three]
-}
-
-export async function updateIssues(id, newIssues) {
-  // This example uses metafields to store the data. For more information, refer to https://shopify.dev/docs/apps/custom-data/metafields.
-  return await makeGraphQLQuery(
-    `mutation SetMetafield($namespace: String!, $ownerId: ID!, $key: String!, $type: String!, $value: String!) {
-    metafieldDefinitionCreate(
-      definition: {namespace: $namespace, key: $key, name: "Tracked Issues", ownerType: PRODUCT, type: $type, access: {admin: MERCHANT_READ_WRITE}}
-    ) {
-      createdDefinition {
-        id
-      }
-    }
-    metafieldsSet(metafields: [{ownerId:$ownerId, namespace:$namespace, key:$key, type:$type, value:$value}]) {
-      userErrors {
-        field
-        message
-        code
-      }
-    }
-  }
-  `,
-    {
-      ownerId: id,
-      namespace: "$app:issues",
-      key: "issues",
-      type: "json",
-      value: JSON.stringify(newIssues),
-    },
-  );
-}
-
-export async function getIssues(productId) {
-  // This example uses metafields to store the data. For more information, refer to https://shopify.dev/docs/apps/custom-data/metafields.
-  const res = await makeGraphQLQuery(
-    `query Product($id: ID!) {
-      product(id: $id) {
-        metafield(namespace: "$app:issues", key:"issues") {
-          value
-        }
-      }
-    }
-  `,
-    { id: productId },
-  );
-
-  if (res?.data?.product?.metafield?.value) {
-    return JSON.parse(res.data.product.metafield.value);
-  }
-}
-
-async function makeGraphQLQuery(query, variables) {
-  const graphQLQuery = {
-    query,
-    variables,
-  };
-
-  const res = await fetch("shopify:admin/api/graphql.json", {
-    method: "POST",
-    body: JSON.stringify(graphQLQuery),
-  });
-
-  if (!res.ok) {
-    console.error("Network error");
-  }
-
-  return await res.json();
-}
-
-function generateId(allIssues) {
-  return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
-}
-
-function validateForm({ title, description }) {
-  return {
-    isValid: Boolean(title) && Boolean(description),
-    errors: {
-      title: !title,
-      description: !description,
-    },
-  };
 }
