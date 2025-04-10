4a784ae1fc4c853688b08bc27d0540a2a3248475
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-10 10:24:16 -0500

    generate issue-tracker-action and issue-tracker-block

---
+++ shopify.d.ts
@@ -0,0 +1,13 @@
+import '/Users/bill/src/github.com/Shopify/example-admin-action-and-block-preact/node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/index.js';
+
+//@ts-ignore
+declare module './extensions/issue-tracker-action/src/ActionExtension.jsx' {
+  const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.render.d.ts');
+  const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.render.d.ts').Api;
+}
+
+//@ts-ignore
+declare module './extensions/issue-tracker-block/src/BlockExtension.jsx' {
+  const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts');
+  const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts').Api;
+}
