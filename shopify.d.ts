8b56c099824d5a3853331b8d8de67435e5245bf9
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-11 01:25:19 -0500

    generate admin-print extension

--- shopify.d.ts
+++ shopify.d.ts
@@ -1,4 +1,10 @@
-import '/Users/bill/src/github.com/Shopify/example-admin-action-and-block-preact/node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/index.js';
+import '/Users/bill/src/github.com/Shopify/example-admin-action-and-block-preact/node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410171803_preact@10.26.5/node_modules/@shopify/ui-extensions/index.js';
+
+//@ts-ignore
+declare module './extensions/admin-print/src/PrintActionExtension.jsx' {
+  const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410171803_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.order-details.print-action.render.d.ts');
+  const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410171803_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.order-details.print-action.render.d.ts').Api;
+}
 
 //@ts-ignore
 declare module './extensions/issue-tracker-action/src/ActionExtension.jsx' {
