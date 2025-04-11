f3dd114ef9bee8d2a25260d8a9745712b8361355
Bill Fienberg <bill.fienberg@shopify.com>
2025-04-11 04:05:30 -0500

    update shopify.d.ts

--- shopify.d.ts
+++ shopify.d.ts
@@ -7,12 +7,6 @@ declare module './extensions/admin-print/src/PrintActionExtension.jsx' {
 }
 
 //@ts-ignore
-declare module './extensions/issue-tracker-block/src/BlockExtension.jsx' {
-  const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts');
-  const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts').Api;
-}
-
-//@ts-ignore
 declare module './extensions/issue-tracker-action/src/ActionExtension.jsx' {
   const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.render.d.ts');
   const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.render.d.ts').Api;
@@ -34,4 +28,10 @@ declare module './extensions/issue-tracker-conditional-action/src/ActionExtensio
 declare module './extensions/issue-tracker-conditional-action/src/condition/shouldRender.js' {
   const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410171803_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.should-render.d.ts');
   const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410171803_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.action.should-render.d.ts').Api;
+}
+
+//@ts-ignore
+declare module './extensions/issue-tracker-block/src/BlockExtension.jsx' {
+  const globalThis: typeof import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts');
+  const shopify: import('./node_modules/.pnpm/@shopify+ui-extensions@0.0.0-2025-07-rc-20250410041844_preact@10.26.5/node_modules/@shopify/ui-extensions/build/ts/surfaces/admin/targets/admin.product-details.block.render.d.ts').Api;
 }
